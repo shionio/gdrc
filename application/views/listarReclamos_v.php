1a8ca6e5740712b1836deb02b74dc195ef3ac679
Angel Gutierrez (bav_agutierrez) <angelgutierrez1983@gmail.com>
2020-07-13 13:47:11 -0400

    modificando la vista de listar reclamos para colocarle el filtro de busqueda segun criterio

--- application/views/listarReclamos_v.php
+++ application/views/listarReclamos_v.php
@@ -4,23 +4,48 @@
 
 <!DOCTYPE html>
 <html lang="en">
-<head>
-	<?= cargar_headers() ?>
-	<?= mostrar_nav_bar() ?>
-	<meta charset="UTF-8">
-	<title>Listando Reclamos</title>
-</head>
-<body>
-	<h1 align="center">Listado de Reclamos</h1>
-	<br><br>
-	<div align="center">
-		<!--<a class=" light-blue darken-3 btn" href="registro">Nuevo</a>-->
-		<a class=" light-blue darken-3 btn" href='<?= base_url() ?>Inicio_controller'>Volver</a>
-	</div>
-<br>
-<br>
+  <head>
+  	<?= cargar_headers() ?>
+  	<?= mostrar_nav_bar() ?>
+  	<meta charset="UTF-8">
+  	<title>Listando Reclamos</title>
+  </head>
+  <body>
+  	<h3 align="center">Seguimiento de Reclamo, según el criterio de búsqueda</h3>
+  	<br>
+  <div class="container">
+    
+    <div class="row">
+      <h5>Criterio de búsqueda</h5>
+      <div class="col l4" id="nacionalidad">
+          Nacionalidad       
+            <p>
+              <label>
+                <input class="nacionalidad" name="nacionalidad" type="radio" value="N" />
+                <span><b>Cedula id Nacionalidad -></b></span>
+              </label>
+            </p>
+            <p>
+              <label>
+                <input class="nacionalidad" name="nacionalidad" type="radio" value="NR" />
+                <span><b>Número Reclamo</b></span>
+              </label>
+            </p>
+            <p>
+              <label>
+                <input class="nacionalidad" name="nacionalidad" type="radio" value="NC" />
+                <span><b>Numero Cuenta</b></span>
+              </label>
+            </p>       
+        </div>
+    </div>
+  </div>
 
-	<table class="striped">
+
+
+	<div class="hide">
+    
+    <table class="striped">
         <thead>
           <tr>
               <th>N° Reclamo</th>
@@ -38,12 +63,17 @@
             <td> <?= $reclamo['num_reclamo']?> </td>
             <td> <?= strtoupper($reclamo['nombre']) ?> </td>
             <td> <?= strtoupper($reclamo['apellido']) ?></td>
-            <td></td>
-            <td></td>
+            <td>  </td>
+            <td> <a class="light-blue darken-3"><i class="material-icons left">edit</i></a></td>
           </tr>
       <?php } ?>
         </tbody>
       </table>
+  </div>
+  <div align="center">
+      <!--<a class=" light-blue darken-3 btn" href="registro">Nuevo</a>-->
+      <a class=" light-blue darken-3 btn" href='<?= base_url() ?>Inicio_controller'>Volver</a>
+    </div>
 
 </body>
-</html>
+</html>   
